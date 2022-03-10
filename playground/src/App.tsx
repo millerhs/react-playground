0da67a590b0981505afb52f1c5dd4b75eac70ac7
Hunter Miller <millerhs@vcu.edu>
2022-03-09 23:43:55 -0500

    Set up basic tab layout

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,26 +1,40 @@
+import TabContext from '@mui/lab/TabContext';
+import TabPanel from '@mui/lab/TabPanel';
+import Grid from '@mui/material/Grid';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+enum TabOptions {
+  Chat = 'Chat',
 }
+
+const App = () => {
+  const [selectedTab, setSelectedTab] = React.useState<TabOptions>(
+    TabOptions.Chat
+  );
+
+  const handleTabChange = (
+    event: React.SyntheticEvent,
+    newValue: TabOptions
+  ) => {
+    setSelectedTab(newValue);
+  };
+
+  return (
+    <Grid container direction='column' sx={{ width: '60vw', padidng: '4rem' }}>
+      <TabContext value={selectedTab}>
+        <Grid container item direction='row'>
+          <Tabs value={selectedTab} onChange={handleTabChange}>
+            <Tab label='Chat' />
+          </Tabs>
+        </Grid>
+        <Grid container item sx={{ height: '100%' }}>
+          <TabPanel value={selectedTab}>Hello</TabPanel>
+        </Grid>
+      </TabContext>
+    </Grid>
+  );
+};
 
 export default App;
